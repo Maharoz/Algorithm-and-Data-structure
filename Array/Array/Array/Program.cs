a74044468286c12ae9f5e270ae88ea0d30e4bfb7
maharoz <mahruj66@gmail.com>
2019-10-05 16:20:17 +0600

    Array created

---
+++ Array/Array/Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating the array
+            int[] numbers = new int[3];
+
+            numbers[0] = 10; //inserting the value at first index
+            numbers[1] = 20; //inserting the value at second index
+            numbers[2] = 30; //inserting the value at third index
+
+
+
+            //Printing the array elements
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
