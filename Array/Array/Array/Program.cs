1c02893d73d0b4c2094bf523d99c3c12175995dd
maharoz <mahruj66@gmail.com>
2019-10-05 16:23:45 +0600

    Object initialization syntax

--- Array/Array/Array/Program.cs
+++ Array/Array/Array/Program.cs
@@ -6,15 +6,10 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            //creating the array
-            int[] numbers = new int[3];
+            //creating the array with object initialization syntax
+            int[] numbers = {10,20,30};
 
-            numbers[0] = 10; //inserting the value at first index
-            numbers[1] = 20; //inserting the value at second index
-            numbers[2] = 30; //inserting the value at third index
-
-
-
+            
             //Printing the array elements
             foreach (int number in numbers)
             {
