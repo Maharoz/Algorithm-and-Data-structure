3191af538ae6d1816c3f4d4fb4b9b1a5d84db3f5
maharoz <mahruj66@gmail.com>
2019-10-05 16:39:06 +0600

    Creating the array class

---
+++ Array/Array/Array/Array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array
+{
+    class Array
+    {
+        private int[] items;
+        public Array(int length)
+        {
+            items = new int[length];
+        }
+
+        public void print()
+        {
+            for(int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+        }
+    }
+}
--- Array/Array/Array/Program.cs
+++ Array/Array/Array/Program.cs
@@ -6,18 +6,8 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            //creating the array with object initialization syntax
-            int[] numbers = {10,20,30};
-
-            
-            //Printing the array elements
-            foreach (int number in numbers)
-            {
-                Console.WriteLine(number);
-            }
-
-            //Printing the length
-            Console.WriteLine("Array length is " + numbers.Length);
+            Array numbers = new Array(3);
+            numbers.print();
         }
     }
 }
