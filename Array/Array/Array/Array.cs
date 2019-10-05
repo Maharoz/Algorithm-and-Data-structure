9efbb62ba77455a60970d282bb4af68a668a9a3a
maharoz <mahruj66@gmail.com>
2019-10-05 16:57:22 +0600

    adiing element

--- Array/Array/Array/Array.cs
+++ Array/Array/Array/Array.cs
@@ -7,14 +7,31 @@ namespace Array
     class Array
     {
         private int[] items;
+        private int count;
         public Array(int length)
         {
             items = new int[length];
         }
 
+        public void insert(int item)
+        {
+            if(items.Length == count)
+            {
+                int[] newItems = new int[count * 2];
+                for(int i = 0; i < count; i++)
+                {
+                    newItems[i] = items[i];
+                }
+
+                items = newItems;
+            }
+            items[count] = item;
+            count++;
+        }
+
         public void print()
         {
-            for(int i = 0; i < items.Length; i++)
+            for(int i = 0; i < count; i++)
             {
                 Console.WriteLine(items[i]);
             }
--- Array/Array/Array/Program.cs
+++ Array/Array/Array/Program.cs
@@ -7,6 +7,12 @@ namespace Array
         static void Main(string[] args)
         {
             Array numbers = new Array(3);
+            numbers.insert(10);
+            numbers.insert(20);
+            numbers.insert(30);
+            numbers.insert(10);
+            numbers.insert(20);
+            numbers.insert(30);
             numbers.print();
         }
     }
