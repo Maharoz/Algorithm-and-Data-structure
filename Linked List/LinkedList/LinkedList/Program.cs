b9249fd6e036f44725a2b57cce4d296eb76c902e
maharoz <mahruj66@gmail.com>
2020-01-03 00:08:36 +0600

    linked list added

---
+++ Linked List/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(10);
+            list.AddLast(20);
+            list.AddLast(30);
+            list.AddFirst(5);
+            Console.WriteLine(list);
+
+            foreach (int num in list)
+            {
+                Console.WriteLine(num);
+            }
+
+
+        }
+    }
+}
