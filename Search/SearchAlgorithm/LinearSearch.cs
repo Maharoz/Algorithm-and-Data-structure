5ee185c8c7900028ba19c7f6df23615aba4e65b3
maharoz <mahruj66@gmail.com>
2020-01-10 02:29:11 +0600

    linear search added

---
+++ Search/SearchAlgorithm/LinearSearch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchAlgorithm
+{
+   public class LinearSearch
+    {
+        public int linearSearch(int[] array, int target)
+        {
+            for(var i = 0; i < array.Length; i++)
+            {
+                if(array[i]== target)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Search/SearchAlgorithm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 7, 1, 3, 6, 5, 4, 8 };
+            var search = new LinearSearch();
+            var index = search.linearSearch(numbers, 8);
+            Console.WriteLine(index);
+        }
+    }
+}
