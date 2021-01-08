c87359b82d2546c5b0981a1c98782f24d0beaf69
Maharoz <mahruj66@gmail.com>
2021-01-08 18:36:39 +0600

    implemented queue with array

---
+++ Queue/MakingQueueUsingArray/MakingQueueUsingArray/ArrayQueue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakingQueueUsingArray
+{
+    public class ArrayQueue
+    {
+        public int[] items;
+        public int rear;
+        public int front;
+        public int count;
+        public ArrayQueue(int capacity)
+        {
+            items = new int[capacity];
+        }
+        public void Enqueue(int item)
+        {
+            if (count == items.Length)
+                throw new Exception();
+            items[rear] = item;
+            rear++;
+            count++;
+        }
+
+        public int dequeue()
+        {
+            var item =  items[front++];
+            items[front] = 0;
+            count--;
+            return item;
+        }
+    }
+}
