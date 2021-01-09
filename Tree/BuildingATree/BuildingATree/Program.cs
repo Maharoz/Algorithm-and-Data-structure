d717c901bf8f020a132a53d7730e07b6c479503e
Maharoz <mahruj66@gmail.com>
2021-01-09 19:03:13 +0600

    Tree implemented

---
+++ Tree/BuildingATree/BuildingATree/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BuildingATree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            tree.insert(7);
+            tree.insert(4);
+            tree.insert(9);
+            tree.insert(1);
+            tree.insert(6);
+            tree.insert(8);
+            tree.insert(10);
+
+
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ Tree/BuildingATree/BuildingATree/Tree.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingATree
+{
+    public class Tree
+    {
+        private class Node
+        {
+            public int value;
+            public Node leftChild;
+            public Node rightChild;
+
+            public Node(int value)
+            {
+                this.value=value;
+            }
+        }
+
+        private Node root;
+        public void insert(int value)
+        {
+            var node = new Node(value);
+            if(root == null)
+            {
+                root = node;
+                return;
+            }
+
+            Node current = root;
+            while (true)
+            {
+                if(value < current.value)
+                {
+                    if(current.leftChild == null)
+                    {
+                        current.leftChild = node;
+                        break;
+                    }
+                    current = current.leftChild;
+                }
+                else
+                {
+                    if (current.rightChild == null)
+                    {
+                        current.rightChild = node;
+                        break;
+                    }
+                    current = current.rightChild;
+                }
+            }
+        }
+
+    }
+}
