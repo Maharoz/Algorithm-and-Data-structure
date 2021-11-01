44f6d10299fc129d70038488d234be9591dd4d8e
Maharoz <mahruj66@gmail.com>
2021-11-02 00:04:51 +0600

    recursion added

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(factorial(3));
+        }
+
+        
+        public static int factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+
+            return n * factorial(n - 1);
+        }
+    }
+}
