0b5b4c98c85718e160eec76642c85cfa90688121
Maharoz <mahruj66@gmail.com>
2022-05-28 01:16:04 +0600

    heap added

---
+++ Heaps/Heaps/Heaps/Heap.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heaps
+{
+   public class Heap
+    {
+        private int[] items = new int[10];
+        private int size;
+
+        public void insert(int value)
+        {
+
+            if (isFull())
+            {
+                throw new Exception();
+            }
+            items[size++] = value;
+
+            //newItem >parent
+            bubbleUp();
+
+        }
+
+        public bool isFull()
+        {
+            return size == items.Length;
+        }
+        private void bubbleUp()
+        {
+            int index = size - 1;
+            //int parentIndex = (index - 1) / 2;
+
+            while (index > 0 && items[index] > items[parent(index)])
+            {
+                swap(index, parent(index));
+                index = parent(index);
+            }
+        }
+        private int parent(int index)
+        {
+            int x = (index - 1) / 2;
+            return (index - 1) / 2;
+        }
+
+        private void swap(int first,int second)
+        {
+            int temp = items[first];
+            items[first] = items[second];
+            items[second] = temp;
+
+        }
+    }
+}
---
+++ Heaps/Heaps/Heaps/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Heaps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Heap h = new Heap();
+            h.insert(10);
+            h.insert(5);
+            h.insert(17);
+            h.insert(4);
+            h.insert(22);
+        }
+    }
+}
