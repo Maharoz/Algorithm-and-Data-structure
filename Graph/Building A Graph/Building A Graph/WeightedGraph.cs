5c665d11ffef0ea659b36024d7335703f0f3d248
Maharoz <mahruj66@gmail.com>
2022-05-29 01:53:00 +0600

    priority queue implementation

---
+++ Graph/Building A Graph/Building A Graph/WeightedGraph.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building_A_Graph
+{
+    public class WeightedGraph
+    {
+
+        private class Node
+        {
+            public string _label;
+            public Node(string label)
+            {
+                _label = label;
+            }
+        }
+
+        private class Edge
+        {
+            private Node from;
+            private Node to;
+            private int weight;
+
+
+            public Edge(Node from , Node to, int weight)
+            {
+                this.from = from;
+                this.to = to;
+                this.weight = weight;
+            }
+
+
+            private Dictionary<string, Node> _nodes = new Dictionary<string, Node>();
+            private Dictionary<string, List<Edge>> _adjacencyList = new Dictionary<string, List<Edge>>();
+
+            //public void addNode(string label)
+            //{
+            //    var node = new Node(label);
+            //    _nodes.Add(label, node);
+            //    _edges.Add(label, new List<Node> { });
+            //}
+            //public void addEdge(string from, string to,int weight)
+            //{
+            //    Node nodeFrom = new Node(from);
+            //    var fromNode = _nodes.ContainsKey(from);
+
+            //    if (fromNode == false)
+            //    {
+            //        throw new Exception();
+            //    }
+
+            //    Node nodeTo = new Node(to);
+            //    var toNode = _nodes.ContainsKey(to);
+
+            //    if (toNode == false)
+            //    {
+            //        throw new Exception();
+            //    }
+
+
+            //    if (_adjacencyList.ContainsKey(from))
+            //    {
+            //        List<Node> previouslyAddedNodes = _adjacencyList[from];
+            //        previouslyAddedNodes.Add(nodeTo);
+            //        _edges[from] = previouslyAddedNodes;
+            //    }
+
+
+            //}
+        }
+    }
+}
---
+++ Queue/PriorityQueue/PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+    public class PriorityQueue
+    {
+        private int[] items = new int[5];
+        private int count;
+        public void add(int item)
+        {
+            if (count == items.Length) {
+                throw new Exception();
+            }
+            int i;
+            for( i = count - 1; i >= 0; i--)
+            {
+                if (items[i] > item)
+                {
+                    items[i + 1]=items[i];
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
+            items[i + 1] = item;
+            count++;
+        }
+
+        public int remove()
+        {
+            if (count == items.Length)
+            {
+                throw new Exception();
+            }
+            return items[--count];
+        }
+    }
+}
