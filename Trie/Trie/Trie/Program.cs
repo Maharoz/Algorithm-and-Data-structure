5247a2ea648959800a399ecd4fc7c1ee53909745
Maharoz <mahruj66@gmail.com>
2022-05-30 01:11:21 +0600

    Tries implemented

---
+++ Trie/Trie/Trie/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Trie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            var trie = new Trie();
+            trie.insert("cat");
+            trie.insert("can");
+        }
+    }
+}
---
+++ Trie/Trie/Trie/Trie.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trie
+{
+    public class Trie
+    {
+        public static int ALPHABET_SIZE = 26;
+        public class Node
+        {
+            public char value;
+            public Node[] children = new Node[ALPHABET_SIZE];
+            public bool isEndOfWord;
+
+            public Node(char value)
+            {
+                this.value = value;
+
+            }
+
+
+        }
+
+        private Node root = new Node(' ');
+
+        
+        public void insert(string word)
+        {
+            Node current = root;
+
+
+            foreach (var ch in word.ToCharArray())
+            {
+                var index = ch - 'a';
+                if (current.children[index] == null)
+                {
+                    current.children[index] = new Node(ch);
+                    
+                }
+                current = current.children[index];
+            }
+
+            current.isEndOfWord = true;
+        }
+    }
+}
