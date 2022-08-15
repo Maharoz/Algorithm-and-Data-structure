6255c3f7bf08c2ffd01a567d9da638bb121bb22a
Maharoz <mahruj66@gmail.com>
2022-08-16 00:27:18 +0600

    string reverse added

---
+++ String/ReverseAString/ReverseAString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = reverse("Hello");
+            Console.WriteLine(x);
+        }
+
+        public static String reverse(string s)
+        {
+            StringBuilder rev = new StringBuilder();
+
+            for(int i = s.Length; i > 0; i--)
+            {
+                rev.Append(s.ToCharArray()[i-1]);
+            }
+            return rev.ToString();
+        }
+    }
+}
