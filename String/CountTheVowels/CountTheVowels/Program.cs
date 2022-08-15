dd9ec735284e0f3cf6711142127d919e25381bf9
Maharoz <mahruj66@gmail.com>
2022-08-16 00:05:26 +0600

    vowel count added

--- String/CountTheVowels/CountTheVowels/Program.cs
+++ String/CountTheVowels/CountTheVowels/Program.cs
@@ -6,7 +6,21 @@ namespace CountTheVowels
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int totalVowel = count("Hello World!".ToLower());
+            Console.WriteLine(totalVowel);
+        }
+        public static int count(string s)
+        {
+            int count = 0;
+            string vowels = "aeiou";
+            foreach(char c in s)
+            {
+                if (vowels.IndexOf(c) != -1)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
