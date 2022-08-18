1cb8395f7d986efab28b1ef3bab06f5240532ea6
Maharoz <mahruj66@gmail.com>
2022-08-18 22:03:32 +0600

    most repeating character

---
+++ String/MostRepeatedCharacter/MostRepeatedCharacter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MostRepeatedCharacter
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var x = getMaxOccurance("Trees are beautiful");
+            Console.WriteLine(x);
+        }
+
+        public static char getMaxOccurance(string s)
+        {
+            const int max = 256;
+            int[] freq = new int[max];
+
+            foreach (var x in s.ToLower().ToCharArray())
+            {
+                freq[x]++;
+            }
+
+            int maxOccurance = 0;
+            char result = ' ';
+            for (var i = 0; i < freq.Length; i++)
+            {
+                if (freq[i] > maxOccurance)
+                {
+                    maxOccurance = freq[i];
+                    result = (char)i;
+                }
+            }
+            return result;
+
+        }
+
+    }
+  
+
+}
+
+
