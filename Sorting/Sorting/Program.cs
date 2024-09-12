b819cd16239b61f548b4982b2f519031df655790
Maharoz <mahruj66@gmail.com>
2024-09-12 10:54:06 +0600

    QuickSort Implemented

--- Sorting/Sorting/Program.cs
+++ Sorting/Sorting/Program.cs
@@ -4,8 +4,8 @@
 	{
 		static void Main(string[] args)
 		{
-			int[] array = new int[] { 9,8,7,6,9,1,5,2 };
-			BubbleSort.bubblesort(array);
+			int[] array = new int[] {6,4,5,1,8,2,9,7,3,10};
+			QuickSort.sort(array);
 			for (int i = 0; i < array.Length; i++) {
 				Console.WriteLine(array[i]);
 			}
---
+++ Sorting/Sorting/QuickSort.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+	public class QuickSort
+	{
+
+		private static void quickSort(int[] arr , int lower,int upper)
+		{
+			if(upper<=lower)
+			{
+				return;
+			}
+
+			int pivot=arr[lower];
+			int start = lower;
+			int stop = upper;
+
+			while (lower < upper)
+			{
+				while (arr[lower]<=pivot && lower < upper)
+				{
+					lower++;
+				}
+				while (arr[upper]>pivot && lower <= upper)
+				{
+					upper--;
+				}
+				if (lower < upper)
+				{
+					swap(arr,upper,lower);
+				}
+			}
+			swap(arr,upper,start);
+			quickSort(arr, start, upper - 1);
+			quickSort(arr, upper+1,stop);
+		}
+
+		public static void sort(int[] arr)
+		{
+			int size =arr.Length;
+			quickSort(arr, 0, size-1);
+		}
+
+		private static void swap(int[] arr, int first, int second) {
+
+			int temp = arr[first];
+			arr[first] = arr[second];
+			arr[second] = temp;
+		}
+	}
+}
