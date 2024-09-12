e9e45bcdba6c8d3a989c9ebc337cc53963361061
Maharoz <mahruj66@gmail.com>
2024-09-12 09:58:46 +0600

    Bubble sort Implemented

---
+++ Sorting/Sorting/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+	public class BubbleSort
+	{
+		private static bool more(int value1,int value2)
+		{
+			return value1 > value2;
+		}
+
+		public static void bubblesort(int[] arr)
+		{
+			int size= arr.Length;
+			int i, j, temp;
+
+			for ( i = 0; i < (size-1); i++) {
+				for (j = 0; j < size - i - 1; j++) {
+					if (more(arr[j], arr[j+1])) { 
+					 temp = arr[j];
+					 arr[j] = arr[j+1];
+						arr[j+1] = temp;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Sorting/Sorting/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Sorting
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = new int[] { 9,8,7,6,9,1,5,2 };
+			BubbleSort.bubblesort(array);
+			for (int i = 0; i < array.Length; i++) {
+				Console.WriteLine(array[i]);
+			}
+		}
+	}
+}
