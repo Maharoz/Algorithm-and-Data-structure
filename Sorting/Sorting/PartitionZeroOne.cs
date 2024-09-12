5301aeb0856a2efa2e053540b5bf661f351c7db7
Maharoz <mahruj66@gmail.com>
2024-09-12 12:22:34 +0600

    Parition 0/1 Implemented

---
+++ Sorting/Sorting/PartitionZeroOne.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+	public class PartitionZeroOne
+	{
+		public static int Partition01(int[] arr, int size)
+		{
+			int left = 0;
+			int right = size - 1;
+			int count = 0;
+
+			while (left < right)
+			{
+				while (arr[left] == 0)
+				{
+					left += 1;
+				}
+				while (arr[right] == 1)
+				{
+					right -= 1;
+				}
+
+				if (left < right)
+				{
+					swap(arr, left, right);
+					count += 1;
+				}
+			}
+			return count;
+		}
+
+		private static void swap(int[] arr, int first, int second)
+		{
+
+			int temp = arr[first];
+			arr[first] = arr[second];
+			arr[second] = temp;
+		}
+	}
+}
--- Sorting/Sorting/Program.cs
+++ Sorting/Sorting/Program.cs
@@ -1,14 +1,14 @@
-﻿namespace Sorting
+﻿using System.Collections.Concurrent;
+
+namespace Sorting
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			int[] array = new int[] {5,6,2,4,7,3,1};
-			SelectionSort.Sort(array);
-			for (int i = 0; i < array.Length; i++) {
-				Console.WriteLine(array[i]);
-			}
+			int[] array = new int[] {0,1,1,0,1,0,1,1,0,0,0,1};
+			int result= PartitionZeroOne.Partition01(array,array.Length);
+		
 		}
 	}
 }
